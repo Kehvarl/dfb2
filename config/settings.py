f435ce45ff48f98250a98367df7a01597367b412
kehvarl <kehvarl@kehvarl.com>
2020-11-22 13:15:46 -0700

    Debug

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'pages.app.PagesConfit',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
